66a55a4559be1e3211e4a134ffc6113a766139a1
Bryan Gaona <bryan@misturatechnologies.com>
2024-11-02 20:01:47 -0500

    feat: initial setup wcf application

---
+++ WcfServiceApp/ConsoleApp/Program.cs
@@ -0,0 +1,17 @@
+﻿// See https://aka.ms/new-console-template for more information
+using ServiceReference1;
+
+Console.WriteLine("Hello, World!");
+ServiceReference1.Service1Client client = new ServiceReference1.Service1Client();
+
+CompositeType compositeType = new CompositeType
+{
+    BoolValue = true,
+    StringValue = "HOLA PRUEA"
+};
+var result = client.GetDataUsingDataContractAsync(compositeType).Result;
+Console.WriteLine("BoolValue: " + result.BoolValue); 
+Console.WriteLine("StringValue: " + result.StringValue);
+
+var hello = client.HelloAsync().Result;
+Console.WriteLine(hello);
---
+++ WcfServiceApp/WcfServiceApp/IProductService.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.ServiceModel;
+using System.Text;
+
+namespace WcfServiceApp
+{
+    // NOTA: puede usar el comando "Rename" del menú "Refactorizar" para cambiar el nombre de interfaz "IProductService" en el código y en el archivo de configuración a la vez.
+    [ServiceContract]
+    public interface IProductService
+    {
+        [OperationContract]
+        void DoWork();
+    }
+    [DataContract]
+    public class Product
+    {
+
+    }
+}
